4c60378ebdd7dadf332b85137710158649f99c52
Pizza <wilderpizza99@gmail.com>
2024-08-01 22:35:18 +0200

    Update manual_install.sh

--- manual_install.sh
+++ manual_install.sh
@@ -2,6 +2,5 @@
 
 cd /usr/local
 sudo rm -r Homebrew
-sudo mkdir homebrew && sudo curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C homebrew
-sudo usr/local/homebrew/bin/brew install wget
-sudo export PATH=/usr/local/homebrew:$PATH
+curl -L https://github.com/Homebrew/homebrew/tarball/master | tar -xzf - -C Homebrew
+sudo export PATH=/usr/local/Homebrew:$PATH
